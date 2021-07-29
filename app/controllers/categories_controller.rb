b8428d851d1490049f24f002463c71d57001262d
micoongkeko <ivongkeko@gmail.com>
2021-07-29 15:24:31 +0800

    Create rspec for update, def show, and create html.erb

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
-    
+    def show
+    end
 
     def new
         @category = Category.new
--- spec/system/creating_categories_spec.rb
+++ spec/system/creating_categories_spec.rb
@@ -17,4 +17,18 @@ RSpec.describe "CreatingCategories", type: :system do
     category = Category.order('id').last
     expect(category.title).to eq('Food')
   end
+
+  it 'edits and saves the category' do
+    visit '/categories/:id'
+
+    click_on 'Edit Category'
+
+    fill_in 'Title', with: 'Snacks'
+    click_on 'Update Category'
+    expect(page).to have_content('Snacks')
+
+    category = Category.order('id')
+    expect(category.title).to eq('Snacks')
+  end
+
 end
