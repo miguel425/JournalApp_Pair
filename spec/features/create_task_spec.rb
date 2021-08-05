465ee14efc7cd624196010b5c6bc3253a97bf524
micoongkeko <ivongkeko@gmail.com>
2021-08-05 20:20:11 +0800

    Created spec file for creating task (system test)

--- spec/features/create_task_spec.rb
+++ spec/features/create_task_spec.rb
@@ -1,5 +1,24 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.feature "CreateTasks", type: :feature do
-#   pending "add some scenarios (or delete) #{__FILE__}"
-# end
+RSpec.feature "CreateTasks", type: :system do
+  before do 
+    driven_by(:rack_test)
+    @category = Category.create(title: 'Food')
+  end
+
+  it 'creates and saves a new task for the category' do
+      visit "/categories/#{@category.id}"
+      
+      fill_in 'Header', with: 'Make lunch!'
+      fill_in 'Description', with: 'Prepare a chicken dish for four people.'
+
+      click_on 'Create Task'
+
+      expect(page).to have_content('Make lunch!')
+      expect(page).to have_content('Prepare a chicken dish for four people.')
+
+      task = @category.task('id').last
+      expect(task.header).to eq('Make lunch!')
+      expect(task.description).to eq('Prepare a chicken dish for four people.')
+  end
+end
