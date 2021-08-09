9d10962c79ced54504a7f29056da36783f715754
micoongkeko <ivongkeko@gmail.com>
2021-08-09 17:05:32 +0800

    Route for delete task fixed

--- spec/features/delete_task_spec.rb
+++ spec/features/delete_task_spec.rb
@@ -1,20 +1,24 @@
 require 'rails_helper'
 
+class DeleteTasksTests < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+end
+
+
 RSpec.feature "DeleteTasks", type: :feature do
   before do 
     @user = User.create!(email: 'test@test.com', password: 'test123')
     @category = @user.categories.create!(title: 'Food')
     @task = @category.tasks.create!(header: "Make lunch", description: "Have a good lunch!")
+    sign_in(@user)
   end
 
   it 'deletes a task from the category show' do
     visit "/categories/#{@category.id}"
 
-    click_on "Delete Task"
-
+    expect { click_on "Delete Task" }.to change { Task.count }.by(-1)
+    
     expect(page).not_to have_content("Make lunch")
     expect(page).not_to have_content("Have a good lunch!")
-
-    expect { @task.destroy }.to change { Task.count }.by(-1)
   end
 end
